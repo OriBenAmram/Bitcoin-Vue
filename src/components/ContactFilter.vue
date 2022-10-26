472515f7d6363f3103b2b47525b9b05bd0ba1abe
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2022-10-26 14:42:29 +0300

    push for pages

--- src/components/ContactFilter.vue
+++ src/components/ContactFilter.vue
@@ -1,36 +1,37 @@
 <template>
-    <section class="contact-filter">
-        <h1 class="filter-header">
-            <span>Filter your friends</span>  by <br>  their names or phone numbers!
-        </h1>
-        <div class="gradient-border"></div>
-        <p class="input-trigger">Don't stay with any open checks....</p>
-        <input
-        class="secondary-input"
-            @input="filter"
-            ref="filter"
-            v-model="filterBy.name"
-            type="text"
-            placeholder="Search by name and phone number"
-        />
-    </section>
+  <section class="contact-filter">
+    <h1 class="filter-header">
+      <span>Filter your friends</span> by <br />
+      their names or phone numbers!
+    </h1>
+    <div class="gradient-border"></div>
+    <p class="input-trigger">Don't stay with any open checks....</p>
+    <input
+      class="secondary-input"
+      @input="filter"
+      ref="filter"
+      v-model="filterBy.name"
+      type="text"
+      placeholder="Search by name and phone number"
+    />
+  </section>
 </template>
 
 <script>
-
 export default {
-    data() {
-        return {
-            filterBy: { name: '' }
-        }
+  data() {
+    return {
+      filterBy: { name: "" },
+    };
+  },
+  methods: {
+    filter() {
+      console.log("this.filterby", this.filterby);
+      this.$emit("filter", this.filterBy);
     },
-    methods: {
-        filter() {
-            this.$emit('filter', this.filterBy)
-        }
-    },
-    mounted() {
-        this.$refs.filter.focus()
-    }
-}
+  },
+  mounted() {
+    this.$refs.filter.focus();
+  },
+};
 </script>
